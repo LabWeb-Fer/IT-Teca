f45a04e186f9426c8b3d6dc61771bcefcda0a758
Hector Fernando Calisaya <205468351+Fe25dev@users.noreply.github.com>
2025-11-23 02:19:08 -0300

    Dev docker (#9)
    * frontend-base-creada-limpia
    
    * frontend-base-creada-limpia
    
    * frontend-TDD pass-Crean Arq
    
    * storybook agregado-bookList loanList-test passed
    
    * storybook agregado-userList-test passed
    
    * coneccion frontend  backend a completar
    
    * frontend conectado a backend-con revisiones
    
    * frontend-backend-release1 beta
    
    * frontend-backend-release1 beta
    
    * frontend-backend-release1 beta
    
    * frontend-backend-release1 beta
    
    * frontend-backend-release1 beta
    
    * frontend-backend-release1 beta
    
    * frontend-backend-release1 beta
    
    * imagenes completadas -back y front -revicion coneccion de mysql
    
    * imagenes completadas -back y front -revicion coneccion de mysql
    
    * agregados archivos configuracion
    
    * agregados archivos configuracion

--- apps/backend/src/db.ts
+++ apps/backend/src/db.ts
@@ -1,15 +1,32 @@
-// apps/backend/src/db.ts
-
-import 'dotenv/config';
+// // apps/backend/src/db.ts
 import { Sequelize } from 'sequelize';
+import 'dotenv/config';
 
-const dbUrl = process.env.DB_URL;
+// Configuración de la base de datos
+const isDocker = process.env.DOCKER_ENV === 'true';
 
-if (!dbUrl) {
-    throw new Error(' La variable de entorno DB_URL no está definida');
+const host = isDocker ? process.env.DB_HOST_DOCKER : process.env.DB_HOST_LOCAL;
+const port = isDocker ? process.env.DB_PORT_DOCKER : process.env.DB_PORT_LOCAL;
+const user = process.env.DB_USER;
+const password = process.env.DB_PASSWORD;
+const dbName = process.env.DB_NAME;
+
+// Verifica que todas las variables necesarias estén definidas
+if (!host || !port || !user || !password || !dbName) {
+  throw new Error('Las variables de entorno de la DB no están definidas');
 }
 
-export const sequelize = new Sequelize(dbUrl, {
-    dialect: 'mysql',
-    logging: false, //  true en debugg
+// Configura la conexión Sequelize con MySQL
+export const sequelize = new Sequelize(`mysql://${user}:${password}@${host}:${port}/${dbName}`, {
+  dialect: 'mysql',
+  logging: false,
 });
+
+// Prueba la conexión
+sequelize.authenticate()
+  .then(() => {
+    console.log('Conexión a la base de datos establecida con éxito');
+  })
+  .catch((error) => {
+    console.error('No se pudo conectar a la base de datos:', error);
+  });
