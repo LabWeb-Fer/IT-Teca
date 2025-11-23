c244f6f33aeb745db81c751df10bf5923462d8a0
Fernando Calisaya <205468351+Fe25dev@users.noreply.github.com>
2025-11-23 04:13:16 -0300

    push

--- apps/backend/index.ts
+++ apps/backend/index.ts
@@ -1,3 +1,4 @@
+
 import 'dotenv/config';
 import Fastify from 'fastify';
 import { sequelize } from './src/db.js';
@@ -11,58 +12,56 @@ import { userRoutes } from './src/routes/userRoutes';
 import { bookRoutes } from './src/routes/bookRoutes';
 import { loanRoutes } from './src/routes/loanRoutes';
 
-
-// async function buildServer() {
-//   const app = Fastify();
-
-//   const allowedOrigins = (process.env.CORS_ORIGINS || 'http://localhost:5173').split(',');
-  
-//   await app.register(cors, {
-//     origin: allowedOrigins, 
-//     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-//   });
-
-
- async function buildServer() {
+async function buildServer() {
   const app = Fastify();
 
-  const allowedOrigins = [
-    'http://localhost:5173', 
-    'http://localhost:6006',
-    // Vitest 
-  ];
+  const allowedOrigins = (process.env.CORS_ORIGINS || 'http://localhost:5173').split(',');
   
-  await app.register(cors, {
-    origin: allowedOrigins, 
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  }); 
+  try {
+
+    await app.register(cors, {
+      origin: allowedOrigins,
+      methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    });
+    console.log('CORS configurado correctamente.');
+  } catch (error) {
+    console.error('Error al registrar CORS:', error);
+    process.exit(1);
+  }
 
   try {
+
     await sequelize.authenticate();
-    await sequelize.sync(); // sincroniza modelos con BD
+    await sequelize.sync(); 
     console.log('Conectado correctamente a la base de datos MySQL');
   } catch (error) {
     console.error('Error al conectar a la base de datos:', error);
     process.exit(1);
   }
 
-  app.register(userRoutes);
-  app.register(bookRoutes);
-  app.register(loanRoutes);
+  try {
+
+    app.register(userRoutes);
+    app.register(bookRoutes);
+    app.register(loanRoutes);
+    console.log('Rutas registradas correctamente.');
+  } catch (error) {
+    console.error('Error al registrar rutas:', error);
+    process.exit(1);
+  }
 
   return app;
 }
 
 buildServer()
   .then(async app => {
+    const port = Number(process.env.PORT) || 3000;
 
-    const port = Number(process.env.PORT) || 3000; 
+    await app.listen({ port: port, host: '0.0.0.0' });
 
-    await app.listen({ port: port });
-
-    console.log(`Servidor corriendo en puerto en: http://localhost:${port}`);
+    console.log(`Servidor corriendo en puerto: http://0.0.0.0:${port}`);
   })
   .catch(err => {
-    console.error(err);
+    console.error('Error al iniciar el servidor:', err);
     process.exit(1);
   });
